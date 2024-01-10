c3dedfc31430b21436bf9ef2a521ea2ecf7b5621
Tristan D Gant <tgant2017@outlook.com>
2024-01-09 22:05:01 -0600

    left and right pointer

---
+++ array_strings/345-reverse-vowels-of-a-string.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/reverse-vowels-of-a-string/submissions/1142039439/?envType=study-plan-v2&envId=leetcode-75
+// *********************************************************************************************************** */
+// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+//*********************************************************************************************************** */
+
+// APPROACH
+// Use a left and right pointer and work in and swap
+
+// NOTE
+// Tried to do too much at first and then simplified my approach
+
+// Time Complexity
+// o(n)
+// Space Complexity
+// o(n)
