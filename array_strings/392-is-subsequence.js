b63c4a2b2a1e635328cd994043736ba50682b527
Tristan D Gant <tgant2017@outlook.com>
2024-01-20 17:23:54 -0600

    two pointer problem with a single loop

---
+++ array_strings/392-is-subsequence.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/is-subsequence/description/
+
+// *********************************************************************************************************** */
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+
+// A subsequence of a string is a new string that is formed from the original string by 
+// deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. 
+// (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+//*********************************************************************************************************** */
+
+// APPROACH
+// Two pointers. Tried using a extra loop at first but then changed it and 
+// did it in one
+
+
+// Time Complexity
+// o(n)
+// Space Complexity
+// o(1)
